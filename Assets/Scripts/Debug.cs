d7aaa49efda1674598b5b006e2f8d03a7826c8f8
cride <gamingbalint80@gmail.com>
2023-12-15 14:33:58 +0100

    Sprites

---
+++ Assets/Scripts/Debug.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug : MonoBehaviour {
+
+    /*
+        80 radius scale = 300 pixel
+        90 radius scale = 330 pixel
+        100 radius scale = 360 pixel
+
+        1 scale = 3 pixel
+    */
+
+    void Start( ) {
+
+    }
+
+    void Update( ) {
+
+    }
+}
