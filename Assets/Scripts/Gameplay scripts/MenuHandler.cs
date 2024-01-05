e2c5b7a60ab37d8244b73ca351430a5bacb8a69d
Stringel <svecovszergej@gmail.com>
2024-01-05 23:45:57 +0100

    Update
    +  Added settings button and Volume for it
    
    ToDo: 
    add a settings to the spawner.cs & MainHandle.cs
    fix when 1 tab opened close another one (Upgrade Menu / Settings Menu)

--- Assets/Scripts/Gameplay scripts/MenuHandler.cs
+++ Assets/Scripts/Gameplay scripts/MenuHandler.cs
@@ -9,6 +9,7 @@ public class MenuHandler : MonoBehaviour {
 
     // dragged in editor
     public GameObject UpgradePanel;
+    public GameObject OptionPanel;
 
     private bool Open = false;
     private Vector3 vecPosition;
@@ -19,15 +20,35 @@ public class MenuHandler : MonoBehaviour {
     (float start, float end) bounds;
     void Start( ) {
 
-        vecPosition = UpgradePanel.transform.localPosition;
-        bounds = (vecPosition.x, vecPosition.x + 830);
+        if (UpgradePanel != null)
+        {
+            vecPosition = UpgradePanel.transform.localPosition;
+            bounds = (vecPosition.x, vecPosition.x + 830);
+        }
+
+        if (OptionPanel != null)
+        {
+            vecPosition = OptionPanel.transform.localPosition;
+            bounds = (vecPosition.x, vecPosition.x - 830);
+        }
     }
 
     void Update( ) {
 
-        float difference = UpgradePanel.transform.localPosition.x - (Open ? bounds.end : bounds.start);
-        float scale = ( 0.05f * ( Mathf.Abs( difference ) / 830f ) );
-        LerpAmount = Mathf.Clamp01( LerpAmount + ( Open ? scale : -scale ) );
-        UpgradePanel.transform.localPosition = new( Mathf.Lerp( bounds.start, bounds.end, LerpAmount ), vecPosition.y, vecPosition.z );
+        if (UpgradePanel != null)
+        {
+            float difference = UpgradePanel.transform.localPosition.x - (Open ? bounds.end : bounds.start);
+            float scale = (0.05f * (Mathf.Abs(difference) / 830f));
+            LerpAmount = Mathf.Clamp01(LerpAmount + (Open ? scale : -scale));
+            UpgradePanel.transform.localPosition = new(Mathf.Lerp(bounds.start, bounds.end, LerpAmount), vecPosition.y, vecPosition.z);
+        }
+
+        if (OptionPanel != null)
+        {
+            float difference = OptionPanel.transform.localPosition.x - (Open ? bounds.end : bounds.start);
+            float scale = (0.05f * (Mathf.Abs(difference) / 830f));
+            LerpAmount = Mathf.Clamp01(LerpAmount + (Open ? scale : -scale));
+            OptionPanel.transform.localPosition = new(Mathf.Lerp(bounds.start, bounds.end, LerpAmount), vecPosition.y, vecPosition.z);
+        }
     }
 }
