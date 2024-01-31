141996c3c959f5c088db88f80200722d3728ed11
Stringel <svecovszergej@gmail.com>
2024-01-31 21:29:58 +0100

    feat(Assets): Add audio control and new sprites
    This update introduces an AudioController script for managing audio settings. It includes a slider for volume control and buttons to toggle the sound on/off.
    
    New sprite assets have been added, including 'Muted' and 'Sound' icons. These are used in the AudioController to visually indicate the current audio state.
    
    The changes also involve updates to various .meta files to accommodate the new scripts and sprites.

---
+++ Assets/Scripts/Options/AudioController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+    public Sprite audioOn;
+    public Sprite audioOff;
+    public GameObject audioButton;
+    public Slider slider;
+
+    public AudioSource audioSource;
+    void Start()
+    {
+        GameObject projectileObject = GameObject.Find("Projectile");
+
+        if (projectileObject != null)
+        {
+            audioSource = projectileObject.GetComponent<AudioSource>();
+        }
+    }
+    void Update()
+    {
+        audioSource.volume = slider.value;
+    }
+
+    public void AudioButton()
+    {
+        if (audioSource.volume == 1)
+        {
+            slider.value = 0;
+            audioButton.GetComponent<Image>().sprite = audioOff;
+        }
+        else
+        {
+            slider.value = 1;
+            audioButton.GetComponent<Image>().sprite = audioOn;
+        }
+    }
+}
