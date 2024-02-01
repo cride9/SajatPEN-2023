7f8e91a5144c9b16b88a5baf499f23e7586f9435
Peter <szilagypet53@gmail.com>
2024-02-02 00:11:10 +0100

    Volume Fix

--- Assets/Scripts/Options/AudioController.cs
+++ Assets/Scripts/Options/AudioController.cs
@@ -3,40 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class AudioController : MonoBehaviour
 {
     public Sprite audioOn;
     public Sprite audioOff;
-    public GameObject audioButton;
-    public Slider slider;
+    public GameObject audioButtonSFX;
+    public GameObject audioButtonMusic;
+    private Slider sfxSlider;
+    private Slider musicSlider;
 
-    public AudioSource audioSource;
+    private AudioSource audioSourceSFX;
+    private AudioSource audioSourceMusic;
     void Start()
     {
-        GameObject projectileObject = GameObject.Find("Projectile");
-
-        if (projectileObject != null)
-        {
-            audioSource = projectileObject.GetComponent<AudioSource>();
-        }
+        sfxSlider = GameObject.Find("SFXSlider").GetComponent<Slider>();
+        musicSlider = GameObject.Find("MusicSlider").GetComponent<Slider>();
     }
     void Update()
     {
-        //audioSource.volume = slider.value;
+        GameObject projectileObject = GameObject.Find("Projectile");
+
+        if (projectileObject is not null)
+        {
+            audioSourceSFX = projectileObject.GetComponent<AudioSource>();
+            if (audioSourceSFX is not null)
+            {
+                audioSourceSFX.volume = sfxSlider.value;
+            }
+        }
+
+        AudioListener mainCameraObject = GameObject.Find("Main Camera").GetComponent<AudioListener>();
+
+        if (mainCameraObject is not null)
+        {
+            audioSourceMusic = mainCameraObject.GetComponent<AudioSource>();
+            if (audioSourceMusic is not null)
+            {
+                audioSourceMusic.volume = musicSlider.value;
+            }
+        }
     }
 
-    public void AudioButton()
+    public void SFXAudioMuteButton()
     {
-        if (audioSource.volume == 1)
+        if (audioSourceSFX.volume == 0)
         {
-            slider.value = 0;
-            audioButton.GetComponent<Image>().sprite = audioOff;
+            sfxSlider.value = 1;
+            audioButtonSFX.GetComponent<Image>().sprite = audioOn;
         }
         else
         {
-            slider.value = 1;
-            audioButton.GetComponent<Image>().sprite = audioOn;
+            sfxSlider.value = 0;
+            audioButtonSFX.GetComponent<Image>().sprite = audioOff;
+        }
+    }
+
+    public void MusicAudioMuteButton()
+    {
+        if (audioSourceMusic.volume == 0)
+        {
+            musicSlider.value = 1;
+            audioButtonMusic.GetComponent<Image>().sprite = audioOn;
+        }
+        else
+        {
+            musicSlider.value = 0;
+            audioButtonMusic.GetComponent<Image>().sprite = audioOff;
         }
     }
 }
